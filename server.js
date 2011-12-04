b4ef620070cab6f79027fb7da57e5a42bfa08d52
Dan Newcome <dnewcome@elevenprime.com>
2011-12-04 03:10:34 -0800

    added a drum kit and mappings for my nanopad. This is mostly to test the latency and feasibility of triggering sounds over the network using midi controller and curl

--- server.js
+++ server.js
@@ -28,12 +28,32 @@ app.get('/play', function( req, res ) {
 	var note = req.query.note;
 	var vel = req.query.vel;
 	console.log( "note: " + note + " vel: " + vel );
+	play_drum( note );
 	res.send( 200 );
 });
 
 function wail() {
 	console.log('wailing');
 	sdlmixer.play('static/wavs/ZENIT_DUB_SIREN_02.wav');	
+}
+
+function play_drum( note ) {
+	switch( note ) {
+	case "24":
+		sdlmixer.play('static/wavs/hiphop/kick.wav');	
+		break;
+	case "28":
+		sdlmixer.play('static/wavs/hiphop/snare.wav');	
+		break;
+	case "2B":
+		sdlmixer.play('static/wavs/hiphop/hh-open.wav');	
+		break;
+	case "3B":
+		sdlmixer.play('static/wavs/hiphop/hh-closed.wav');	
+		break;
+	default:
+		console.log( "unsupported note" );
+	}
 }
 
 app.listen(process.env.NODE_ENV === 'production' ? 80 : 3003, function() {
