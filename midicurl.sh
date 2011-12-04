4889ce4aed8ceefa27073ba4097df042e79f93ea
Dan Newcome <dnewcome@elevenprime.com>
2011-12-04 03:11:20 -0800

    added some hints to midi curl script - there are hacks here for my broken midi controller, sorry.

--- midicurl.sh
+++ midicurl.sh
@@ -6,6 +6,14 @@
 # 80 is midi note on, 90 is midi note off
 # for now we ignore note off
 
-echo $1
 tokens=( $1 )
-curl "http://localhost:3003/play?note=${tokens[1]}&vel=${tokens[2]}"
+
+## filter out spurious notes from my broken nanopad, sorry for this
+## cruft.
+if [ ${tokens[1]} == "26" ] ; then exit ; fi
+
+## since this is a drum sampler for now, we only care about note-on events
+if [ ${tokens[0]} == "90" ] ; then 
+	echo $1
+	curl "http://localhost:3003/play?note=${tokens[1]}&vel=${tokens[2]}"
+fi
